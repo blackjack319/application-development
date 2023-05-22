6fa258f2be3f2f91272a3470ab07c718cb9ee051
blackjack319 <animeastro19@gmail.com>
2023-05-22 12:47:06 +0800

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -1,18 +1,12 @@
 import re
 
-def validation(password):
-  pass_regex = '^(?=\S{12,10000}$)(?=.*?\d)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[^A-Za-z\s0-9])'
-  compiled_regex = re.compile(pass_regex)
-  if (re.search(compiled_regex, password)):
-    return True
-  else:
-    return False
-
-def validate():
+def validation():
+    pass_regex = '^(?=\S{12,10000}$)(?=.*?\d)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[^A-Za-z\s0-9])'
+    compiled_regex = re.compile(pass_regex)
     while True:
         password1 = input("Enter a password: ")
         password2 = input("Re-enter your password: ")
-      if (validation(password1)):
+        if (re.search(compiled_regex, password1)):
             if password1 == password2:
                 print("Password is valid!")
                 break
@@ -21,4 +15,4 @@ def validate():
         else:
             print("Password does not meet the requirements. Please try again.")
                    
-validate()
+validation()
