314eb212ec8c642abd1c25f7d0d8c26494c91d2b
blackjack319 <animeastro19@gmail.com>
2023-05-22 12:52:48 +0800

    Update assignment3v1.py

--- assignment3v1.py
+++ assignment3v1.py
@@ -3,8 +3,7 @@ import re
 def validation(password):
     pass_regex = '^(?=\S{12,10000}$)(?=.*?\d)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[^A-Za-z\s0-9])'
     compiled_regex = re.compile(pass_regex)
-    while True:
-        return re.search(compiled_regex, password)
+    return re.search(compiled_regex, password)
 
 def validate():
     while True:
